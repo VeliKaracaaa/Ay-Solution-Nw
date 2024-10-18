560a296384a081b95cd49f355edfb12f9b2eef5b
krc <71193191+VeliKaracaaa@users.noreply.github.com>
2024-10-18 19:13:17 +0200

    making mobile naviagtion work

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,8 +1,10 @@
+// make mobile navigation work
 document.querySelector(".hamburger").addEventListener("click", () => {
     document.querySelector(".menu-outline").classList.toggle("hidden");
     document.querySelector(".close-outline").classList.toggle("hidden");
   });
 
 document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('hidden');
+  document.querySelector('.nav-links').classList.toggle('nav-open');
 });
+
