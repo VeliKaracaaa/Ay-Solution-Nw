f1362bf2c391a3222efb817c112d49108ee7cded
krc <71193191+VeliKaracaaa@users.noreply.github.com>
2024-10-18 16:31:14 +0200

    Bulding Navigation mobile

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,8 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".menu-outline").classList.toggle("hidden");
+    document.querySelector(".close-outline").classList.toggle("hidden");
+  });
+
 document.querySelector('.hamburger').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('hidden');
 });
