1871586870480e036e18c745649b771409c1c2bb
krc <71193191+VeliKaracaaa@users.noreply.github.com>
2024-10-18 20:52:09 +0200

    test

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -7,4 +7,3 @@ document.querySelector(".hamburger").addEventListener("click", () => {
 document.querySelector('.hamburger').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('nav-open');
 });
-
